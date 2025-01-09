9d5d019f1222588ddd58f453d1fa09c6883a3ba8
ANUP MANEKAR <manekaranup393@gmail.com>
2025-01-09 13:40:47 +0530

    This is the introduction to debugging tools in intellij idea

---
+++ src/section_05_introduction_to_java/Example2.java
@@ -0,0 +1,24 @@
+package section_05_introduction_to_java;
+
+import java.lang.*;
+
+public class Example2{
+
+    public static void main(String[] args) {
+        System.out.println("Name: Anup Manekar " +
+                "City: Nagpur ");
+        System.out.println("Mobile: 88303055XX");
+        System.out.println("Pincode: 4400XX");
+        System.out.println("Email: anupmanekarxxx@gmail.com");
+        System.out.println("College: YCCE");
+        System.out.println("Year of graduation: 2025");
+        System.out.println("Department: Electronics and Telecommunication Engineering ");
+        System.out.println("Interest: PLaying Cricket and delivering Commentary");z
+
+
+    }
+}
+
+
+
+
